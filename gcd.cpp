9eba8a94c9367aa5f770ad7afc4fea998c543332
JGalofaro <galo9664@fredonia.edu>
2011-01-26 21:43:23 -0500

    Section 1.1

---
+++ gcd.cpp
@@ -0,0 +1,46 @@
+/**
+ * Greatest Common Divisor
+ *
+ * Author: Josh Galofaro
+ * Date: 26/01/2011
+ *
+ * Design and Analysis of Algorithms, Spring 2011
+ * Section 1.1 of "Introduction to The Design & Analysis of Algorithms"
+ */
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int gcd( int m, int n );
+
+int main( int argc, char * argv[] )
+{
+    if( argc == 3 )
+    {
+        int m, n;
+        m = atoi(argv[1]);      //Convert the arguments to integers
+        n = atoi(argv[2]);
+        cout << gcd(m, n) << endl;
+    }
+    else 
+    {
+        cout << "Error, Command is: ./gcd <num 1> <num 2>\n";
+    }
+}
+
+int gcd( int m, int n )
+{
+    if( n == 0 )
+    {
+        return m;
+    }
+    else 
+    {
+        int r = m / n;
+        m = n;
+        n = r;
+    }
+    
+    return gcd(m, n);
+}
